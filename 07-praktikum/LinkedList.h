e0a0a4afe78ec56a5115b72cee72a6f67d7d1b1c
Max <maximilian.drude002@stud.fh-dortmund.de>
2022-11-23 17:52:11 +0100

    p7 finished

--- 07-praktikum/LinkedList.h
+++ 07-praktikum/LinkedList.h
@@ -11,15 +11,16 @@ public:
         this->head = nullptr;
     }
 
-    int append(const char* data);
+    int append(char* data);
+    int insert(char* data, int p);
     int remove (int p);
-    //const char *get(int p);
-    //int index_of(const char* data);
-    //const char *first();
-    //const char *last();
+    const char *get(int p);
+    int index_of(const char* data);
+    const char *first();
+    const char *last();
     void printAll();
     void addElement(char* data);
-    //void visit_all((*work)(const char* data));
+    void visit_all(void (*work)(const char* data));
 };
 
 #endif //INC_07_PRAKTIKUM_LINKEDLIST_H
--- 07-praktikum/main.cpp
+++ 07-praktikum/main.cpp
@@ -9,10 +9,14 @@ int main() {
     char test1[] = "Test1";
     char test2[] = "Test2";
     char test3[] = "Test3";
+    char test4[] = "append";
+    char test5[] = "insert";
 
     list.addElement(test1);
     list.addElement(test2);
     list.addElement(test3);
+    list.append(test4);
+    list.insert(test5, 2);
     list.printAll();
 
     return 0;
